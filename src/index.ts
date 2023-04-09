24e2a51b858acf11324ff7f63003ff14d3cbbaa3
Patrick Dung <38665827+patrickdung@users.noreply.github.com>
2023-04-09 18:29:26 +0800

    try to fix with newer shiki

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,10 @@ export const AsciidoctorShiki: Asciidoctor.SyntaxHighlighterFunctions = {
   },
   format (node, lang, opts) {
     if (lang) {
-      //return `<pre style="background-color: #222222;">
-      //  <code ${lang ? ` data-language="${lang}"` : ''}>${node.getContent()}</code></pre>`
-      return `<pre>${node.getContent()}</pre>`
+      return `<pre style="background-color: #222222;">
+        <code>${node.getContent()}</code></pre>`
     } else {
-      return ${node.getContent()}
+      return `<pre>${node.getContent()}</pre>`
     };
   },
   highlight(node, content, lang, opts) {

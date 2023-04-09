7667a738bb291c80375f5ee6386c7f38477ac4d2
Patrick Dung <38665827+patrickdung@users.noreply.github.com>
2023-04-09 19:11:07 +0800

    try to fix again

--- dist/worker.js
+++ dist/worker.js
@@ -1,10 +1,10 @@
 // src/worker.ts
 var import_shiki = require("shiki");
 var import_synckit = require("synckit");
-var START_RX = /<pre class="shiki" style="background-color: #[0-9a-f]*"><code>/;
+var START_RX = /<pre class="shiki*" style="background-color: #[0-9a-f]*"><code>/;
 var END = "</code></pre>";
 (0, import_synckit.runAsWorker)(async (content, lang, opts) => {
-  const highlighter = await (0, import_shiki.getHighlighter)({ "theme": "" });
+  const highlighter = await (0, import_shiki.getHighlighter)({ "theme": "slack-dark" });
   let lineOptionsItems = [];
   let html = "";
   if (opts.highlight_lines) {

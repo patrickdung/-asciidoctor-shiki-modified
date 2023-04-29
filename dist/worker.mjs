13e8f7b3a75ca1d5bb2f6c6338e2b18adbfc4fed
Patrick Dung <38665827+patrickdung@users.noreply.github.com>
2023-04-29 17:22:00 +0800

    update worker.ts

--- dist/worker.mjs
+++ dist/worker.mjs
@@ -1,7 +1,7 @@
 // src/worker.ts
 import { getHighlighter } from "shiki";
 import { runAsWorker } from "synckit";
-var START_RX = /<pre id="*" class="shiki*" style="background-color: #[0-9a-f]*"><code>/;
+var START_RX = /<pre id="*" class="shiki*" style="background-color: #[0-9a-f]*"*>*<code>/;
 var END = "</code></pre>";
 runAsWorker(async (content, lang, opts) => {
   const highlighter = await getHighlighter({ "theme": "slack-dark" });
